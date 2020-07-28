1882a00611bcfc48736fb507c1098b006758c57a
Vedaiyappan Suruttaiyan <vedaiyappans@stellarit.com>
2020-07-28 16:39:46 +0530

    CRUD-using razor

--- Pages/BookList/index.cshtml.cs
+++ Pages/BookList/index.cshtml.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Pages.BookList
 {
     public class indexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        public indexModel(ApplicationDbContext db)
         {
-
+            _db = db;
         }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+
     }
 }
